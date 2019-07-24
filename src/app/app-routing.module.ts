d9fbf194f3755df0f1ef3fd0e9df5a1e3d5abc10
Liiioooo <preissegger.leo@a1.net>
2019-07-24 17:36:18 +0200

    update to angular version 8.1.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './modules/home/home.module#HomeModule'}
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)}
 ];
 
 @NgModule({
